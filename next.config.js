d2826d9891e10650d0364d33b31ee85f8295fd1c
vinu1A23 <tan2002vinu@gmail.com>
2024-03-18 23:30:02 +0530

    adding cors

--- next.config.js
+++ next.config.js
@@ -4,10 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
+        destination:"/api/"
       },
     ]
   },
